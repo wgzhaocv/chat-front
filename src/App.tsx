3f2bfaa45be474207d3dee57fa7c52ede34f4344
yumemidori <wgzhaocv@gmail.com>
2023-02-20 00:05:00 +0900

    toast message

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef } from "react";
+import "./App.css";
+import { io } from "socket.io-client";
+import { toast } from "react-toastify";
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+  useEffect(() => {
+    const socket = io();
+    socket.on("greeting", (data) => {
+      toast(data);
+    });
+    fetch("/api/items")
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, []);
+  return <div className="App"></div>;
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+    <>
     <App />
-  </React.StrictMode>
+        <ToastContainer/>
+    </>
 );
 
 // If you want to start measuring performance in your app, pass a function
